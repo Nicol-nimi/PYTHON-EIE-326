15e32daf228522f60cd2195aedca269372ba49ad
Nicol-nimi <145157263+Nicol-nimi@users.noreply.github.com>
2025-05-19 00:44:24 +0100

    Add files via upload

---
+++ GPA CALC.py
@@ -0,0 +1,24 @@
+possible_grades = {
+    'A': 5,
+    'B': 4,
+    'C': 3,
+    'D': 2,
+    'F': 0}
+
+total = 0
+total_units_offered= 0
+
+for i in range(2):
+    grade = input(f"Grade in course {i+1} (A-F): ").upper()
+    units = int(input(f"Units for course {i+1}: "))
+
+    if grade in possible_grades:
+        total += possible_grades[grade] * units
+        total_units_offered += units
+    else:
+        print("Invalid grade entered. Please use A, B, C, D, or F.")
+        break
+
+if total > 0:
+    gpa = total / total_units_offered
+    print("Your GPA is:", round(gpa, 2))
